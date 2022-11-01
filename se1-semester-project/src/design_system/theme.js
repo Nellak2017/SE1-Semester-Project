9b0ec9fbf1d1291277e87e9e08967e968b743c62
Connor Keenum <Nellakprime@gmail.com>
2022-11-01 00:25:51 -0500

    Made elevation values and added base styles to css globals. Now we should have decent defaults for the most part.

--- se1-semester-project/src/design_system/theme.js
+++ se1-semester-project/src/design_system/theme.js
@@ -1,3 +1,18 @@
+
+// see also: https://vaadin.com/docs/latest/styling/lumo/design-tokens/elevation#:~:text=Elevation%20is%20used%20to%20indicate,are%20applied%20using%20box%2Dshadow.
+const elevations = [
+  '0 1px 4px -1px hsla(214, 45%, 20%, 0.52)',
+  '0 2px 4px -1px hsla(214, 53%, 23%, 0.16), 0 3px 12px -1px hsla(214, 50%, 22%, 0.26)',
+  '0 2px 6px -1px hsla(214, 53%, 23%, 0.16), 0 8px 24px -4px hsla(214, 47%, 21%, 0.38)',
+  '0 3px 18px -2px hsla(214, 53%, 23%, 0.16), 0 12px 48px -6px hsla(214, 47%, 21%, 0.38)',
+  '0 4px 24px -3px hsla(214, 53%, 23%, 0.16), 0 18px 64px -8px hsla(214, 47%, 21%, 0.38)']
+elevations.extraSmall = elevations[0] // elements closest to application background, like cards
+elevations.small = elevations[1] // tooltips
+elevations.medium = elevations[2] // contextual overlays for components, like select and menu bar
+elevations.large = elevations[3] // element that rise above most, like dialouges
+elevations.extraLarge = elevations[4] // elements highest in stacking order, like notifications
+
+// see also: https://zaat.dev/blog/building-a-design-system-in-react-with-styled-components/
 const fontSizes = ['10px', '12px', '14px', '16px', '24px', '40px', '64px']
 fontSizes.extraSmall = fontSizes[0]
 fontSizes.smaller = fontSizes[1]
@@ -8,10 +23,10 @@ fontSizes.larger = fontSizes[5]
 fontSizes.extraLarge = fontSizes[6]
 
 const breakpoints = ['600px', '900px', '1200px', '1800px']
-breakpoints.sm = breakpoints[0]
-breakpoints.md = breakpoints[1]
-breakpoints.lg = breakpoints[2]
-breakpoints.xl = breakpoints[3]
+breakpoints.sm = breakpoints[0] // phones
+breakpoints.md = breakpoints[1] // ipads
+breakpoints.lg = breakpoints[2] // laptops
+breakpoints.xl = breakpoints[3] // desktops
 
 const spaces = ['4px', '8px', '16px', '32px', '48px', '56px']
 spaces.smaller = spaces[0]
@@ -21,6 +36,7 @@ spaces.large = spaces[3]
 spaces.larger = spaces[4]
 spaces.extraLarge = spaces[5]
 
+// body and defaultFontColor are redundant and only there for understandability
 export default {
   colors: {
     primaryLight: '#f4f0ff',
@@ -82,9 +98,12 @@ export default {
     warningDark: '#ae8c5b',
     warningDarkHover: '#8b7049',
     warningDarkActive: '#685436',
-    warningDarker: '#51412a'
+    warningDarker: '#51412a',
+    body: '#39313a',
+    defaultFontColor: '#f4f0ff'
   },
   fontSizes,
   breakpoints,
-  spaces
+  spaces,
+  elevations
 }
