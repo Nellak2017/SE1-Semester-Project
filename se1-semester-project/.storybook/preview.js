85e09e62921c142e9d69fc7291724040475ce54e
Connor Keenum <Nellakprime@gmail.com>
2022-11-06 00:33:44 -0500

    Added Decorator for Theme and Global Styles, so all Stories have access to it.

--- se1-semester-project/.storybook/preview.js
+++ se1-semester-project/.storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from '@storybook/react'
+import theme from '../src/design_system/theme'
+import GlobalStyle from '../src/design_system/globalStyles'
+import { ThemeProvider } from 'styled-components'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +12,5 @@ export const parameters = {
     },
   },
 }
+// Wrap every story in the story book with a ThemeProvider and the GlobalStyles
+addDecorator(story => <ThemeProvider theme={theme}><GlobalStyle />{story()}</ThemeProvider>)
