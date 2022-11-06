940440410a98612ae2b1f53d02581a4c05f030c8
Connor Keenum <Nellakprime@gmail.com>
2022-11-05 20:07:04 -0500

    Made the pillbutton in a functional way

--- se1-semester-project/src/components/PillButton/PillButton.elements.js
+++ se1-semester-project/src/components/PillButton/PillButton.elements.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
-import { variant } from 'styled-system'
+import { space, layout, typography } from 'styled-system'
+import { getPresetCSS, buttonPreSets } from '../../design_system/theme'
 
 // This will inherit basic styles from global css, and make new ones too for the component
 // see also: https://styled-system.com/api#variant
@@ -8,22 +9,10 @@ import { variant } from 'styled-system'
 // Todo: Re-write component style to incorporate Theme instead of defaults
 
 export const PillButtonStyled = styled.button`
-    // Styles shared by all variants, should theoretically be in Default CSS
-    color: white; // given by default CSS or Theme
-    border-radius: 32px; // large
-    border: 0px solid transparent;
-    background-color: #9068FE; // based on theme
-
-    // Variant Specific Styles
-    ${variant({
-        variants: {
-            default: {
-                // change color, space, typography, layout, etc. with props passed in
-                padding: '16px 32px'
-            },
-            small: {
-                padding: '8px 16px'
-            }
-        }
-    })}
+    ${space}
+    ${layout}
+    ${typography}
+    ${getPresetCSS(buttonPreSets, 'size')}
+    ${getPresetCSS(buttonPreSets, 'color')}
+    ${getPresetCSS(buttonPreSets, 'variant')}
 `
