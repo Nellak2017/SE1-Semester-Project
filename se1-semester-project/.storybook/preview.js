01bd584ba79bd4678e9a24cb9a79f74cdec4d2a0
Connor Keenum <Nellakprime@gmail.com>
2022-11-08 02:29:32 -0600

    Added Viewport addon to storybook for easier media queries.

--- se1-semester-project/.storybook/preview.js
+++ se1-semester-project/.storybook/preview.js
@@ -1,7 +1,8 @@
-import { addDecorator } from '@storybook/react'
+import { addDecorator, addParameters } from '@storybook/react'
 import theme from '../src/design_system/theme'
 import GlobalStyle from '../src/design_system/globalStyles'
 import { ThemeProvider } from 'styled-components'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -14,3 +15,8 @@ export const parameters = {
 }
 // Wrap every story in the story book with a ThemeProvider and the GlobalStyles
 addDecorator(story => <ThemeProvider theme={theme}><GlobalStyle />{story()}</ThemeProvider>)
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+})
